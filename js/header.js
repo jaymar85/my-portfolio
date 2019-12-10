90c341c837cfbccafd726988109c779f11bf2aab
Jeremy Mark <jeremymark@Christinas-MBP.home>
2019-12-10 09:51:18 -0600

    updated portfolio

---
+++ js/header.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', type);
+  
+const dataText = ['Full Stack Web Developer'];
+  
+function type(text, index) {
+  if (index < text.length) {      
+    document.getElementById('typewriter').innerHTML = text.substring(0, index + 1) + '<span aria-hidden="true"></span>';
+    setTimeout(function() {
+      type(text, index + 1)
+    }, 150);
+  } else if (index === text.length) {
+    document.getElementById('typewriter').innerHTML = text.substring(0, index + 1)  + '<span aria-hidden="true"></span>';
+    cancelTimeout(type) 
+  }
+}
+
+function StartTextAnimation(i) {
+  if (i < dataText[i].length) {
+    type(dataText[i], 0, function(){
+      StartTextAnimation(i + 1);
+    });
+  }
+}
+
+StartTextAnimation(0);
