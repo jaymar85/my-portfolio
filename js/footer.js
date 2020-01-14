33b82ad0d9b4f1ff1f7f74a279e42cf5d09cc2d6
Jeremy Mark <jermark851@gmail.com>
2020-01-14 01:43:24 -0600

    added social icons animation in footer

--- js/footer.js
+++ js/footer.js
@@ -1,10 +1,10 @@
 // const statement = 'Beam me up, Scotty!'
 
-function showText(text){
+function showText(text) {
   document.getElementById("to-top-copy").style.opacity = "1";
   document.getElementById("to-top-copy").style.transition = "0.3s ease-in";
 }
 
-function hide(){
+function hide() {
   document.getElementById("to-top-copy").style.opacity = '0';
 }
