561c216e1d8d6d7ec35e6d9edabb1db8cbc53650
jaymar85 <jermark851@gmail.com>
2020-06-25 20:41:15 -0500

    typewriter

--- js/header.js
+++ js/header.js
@@ -1,22 +1,24 @@
-document.addEventListener('DOMContentLoaded', type);
-  
-const dataText = ['Full Stack Web Developer'];
-  
+document.addEventListener("DOMContentLoaded", type);
+
+const dataText = ["Web Developer", "Creator", "Designer"];
+
 function type(text, index) {
-  if (index < text.length) {      
-    document.getElementById('typewriter').innerHTML = text.substring(0, index + 1) + '<span aria-hidden="true"></span>';
-    setTimeout(function() {
-      type(text, index + 1)
+  if (index < text.length) {
+    document.getElementById("typewriter").innerHTML =
+      text.substring(0, index + 1) + '<span aria-hidden="true"></span>';
+    setTimeout(function () {
+      type(text, index + 1);
     }, 150);
   } else if (index === text.length) {
-    document.getElementById('typewriter').innerHTML = text.substring(0, index + 1)  + '<span aria-hidden="true"></span>';
-    cancelTimeout(type) 
+    document.getElementById("typewriter").innerHTML =
+      text.substring(0, index + 1) + '<span aria-hidden="true"></span>';
+    cancelTimeout(type);
   }
 }
 
 function StartTextAnimation(i) {
   if (i < dataText[i].length) {
-    type(dataText[i], 0, function(){
+    type(dataText[i], 0, function () {
       StartTextAnimation(i + 1);
     });
   }
